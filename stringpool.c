6187b815028a8f68946070d155c9c6ed538fb014
Amanieu d'Antras <amanieu@gmail.com>
2010-05-18 20:18:03 +0100

    More stats

--- stringpool.c
+++ stringpool.c
@@ -7,3 +7,9 @@ struct string_pool_t *string_pool[STRING_TABLE_SIZE];
 // Current memory block used for string allocation
 void *string_mem;
 int string_mem_offset;
+
+#ifdef MEMORY_STATS
+// Amount of memory used by string pool
+int string_mem_usage;
+int string_pool_count;
+#endif
